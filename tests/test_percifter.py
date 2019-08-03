d440306d30beff47e30cdf9d58c5218bc4b21235
SurgeArrester <cameron.h@rgreaves.me.uk>
2019-08-03 15:55:57 +0100

    Added licence, numeric tests and cleaned up slightly

--- tests/test_percifter.py
+++ tests/test_percifter.py
@@ -1,5 +1,24 @@
-#test for percifter
+'''
+A suite of test files to ensure functionality of Percifter
+
+Copyright (C) 2019  Wenkai Zhang, Cameron Hargreaves
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+--------------------------------------------------------------------------------
+'''
+
 import unittest
+
 
 class TestStringMethods(unittest.TestCase):
 
@@ -17,6 +36,16 @@ class TestStringMethods(unittest.TestCase):
         with self.assertRaises(TypeError):
             s.split(2)
 
-suite = unittest.TestLoader().loadTestsFromTestCase(TestStringMethods)
-unittest.TextTestRunner(verbosity=2).run(suite)
+class TestNumericMethods(unittest.TestCase):
+    def test_isnumeric(self):
+        self.assertIsInstance(1, int)
+
+    def test_lt(self):
+        self.assertLess(1, 2)
+
+    def test_gt(self):
+        self.assertGreater(2, 1)
+
+if __name__ == '__main__':
+    unittest.main()
 
